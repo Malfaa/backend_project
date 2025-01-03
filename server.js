e30a7efec301ad8d5f7465f0a7337f62cba181a3
marufa <gmalfacorrea@gmail.com>
2025-01-02 22:12:21 -0300

    teste

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 const host = "localhost";
 const port = 8080;
 
 //Rota simples
-// app.get("/", (req, res) => {
-//     res.send("Hello World!");
-// })
+app.get("/", (req, res) => { // Tentativa de colocar para abrir o index.html padrão
+    // res.send("Hello World!");
+    const filePath = path.join(__dirname,'pages', "index.html");
+    res.sendFile(filePath);
+})
 
 
-app.
+
 
 app.listen(port, host, ()=>{
     console.log(`Servidor aberto na porta: ${port}`);
